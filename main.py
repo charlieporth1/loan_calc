b1e1171bb4644182ca6ddc1c38d0691ec145d7af
Charlie Porth <charlieporth1@gmail.com>
2020-01-28 17:01:57 -0600

    First Commit

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 #!"C:\Python27\python.exe"
-# Import the necessary packages
-#from consolemenu import *
-#from consolemenu.items import *     
-from pimento import menu
-from tabulate import tabulate
-import qprompt
-#global amount
-#global rate
 yn = ["y", "n"]
-def showMenu(amount, rate):
-        title = """ 
-        --------------------------------
-        Amy's Auto - Loan Report Menu
-        --------------------------------
-        """
-        loan_opt = ["1: 12-month loan", "2: 24-month loan", "3: 36-month loan", "4: 48-month loan", "5: 60-month loan", "6: EXIT"]
+def showMenu():
+    title = """ 
+    --------------------------------
+    Amy's Auto - Loan Report Menu
+    --------------------------------
+    """
+    loan_opt = ["1: 12-month loan", "2: 24-month loan", "3: 36-month loan", "4: 48-month loan", "5: 60-month loan", "6: EXIT"]
 
-        print(title)
-        result = menu(loan_opt)
-        if len(loan_opt) == result: 
-                exit()
-        payment(rate, int(result[:1]), amount) 
- #      print(result)
+    print(title)
+    choice = 10
+    while choice > 5: 
+         print('\n1. 12-month loan')
+         print('2. 24-month loan')
+         print('3. 36-month loan')
+         print('4. 48-month loan')
+         print('5. 60-month loan')
+         print('0. EXIT')
+         choice = int(input('Choice:'))
+
+    return choice*12
 def inputLoanData(): 
-        amount = truncate(float(qprompt.ask_str("Enter loan amount:", blk=False)))
-        rate = truncate(float(qprompt.ask_str("Enter annual interest rate:", blk=False)))
-        print("Continue? Y/N")
-        result = menu(yn)
-        if (result == 1): 
-               exit()
-        else:
-               showMenu(amount, rate)
+        amount = truncate(float(input('Enter loan amount: ')))
+        rate = truncate(float(input('Enter annual interest: ')))
+  
+        return amount, rate
 def payment(rate, o, pv):
    p = 0
    try:
@@ -46,36 +40,32 @@ def payment(rate, o, pv):
    intAmt = r * pv
    balance = pv - pmtAmt
    price = pmtAmt - intAmt
-#      table = []
-#       payments = []
-#       ints = [p]
-#       price = [pv]
-#       balance = [pv]
-#       payment_amount = [pv]
-#       for i in range(n):
-#                payments.append(i)
-#                payment_amount.append(price[len(price) - 1] + ints[len(ints) - 1])
-
-#       table = [["Payment \#:", payments], ["Payment Amount: ", payment_amount]]
-       #               print(payments, payment_amount)
-#       table = [["pmtAmt", pmtAmt], ["intAmt",intAmt] , ["balance", balance], ["price", price], ["total", pv + balance] ]
    table = [["Payment Amount", pv + balance]]
-   result = menu(yn)
-   if (result == 1): 
-       exit()
-   else:
-       showReport(table) 
-def showReport(table):
-        print("")
-        print(tabulate(table))
-        result = menu(yn)
-        print("Would you like to restart")
-        if (result == 1):
-               exit()
-        else:
-               inputLoanData()
+   return pmtAmt
+#   showReport(table) 
+def showReport(pv, ratePeriod, numPeriod, pmtAmt):
+    print("Pmt#\t", "PmtAmt\t", "Int\t", "Princ\t", "Balance\t")
+    print("----", "---------","-----", "------", " --------")
+
+
+        
+    for row in range(1,13):
+        print(row, end="\t")
+        print(truncate(pmtAmt), end="\t")
+        
+        interest = truncate(pv * ratePeriod)
+        print(interest, end="\t")
+         
+
+        print(truncate((pmtAmt - interest)), end="\t")
+
+        print(truncate((pv - pmtAmt)), end="\n")
 def truncate(n):
         return round(n, 2)
 def main():
-        inputLoanData()
+        loanAmount, loanInterest = inputLoanData()
+        numOfmon = showMenu()
+        if numOfmon > 0:
+                pmt = payment(loanInterest, numOfmon, loanAmount)
+                showReport(loanAmount,(loanInterest / (100*12)), numOfmon, pmt)
 main()
